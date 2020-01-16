1318289769bd6b84a8fe545be9b59cd0a7b10f68
Glaudson Silva <glaudson.silva@qwti.com.br>
2020-01-16 18:07:35 -0300

    backend

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+require('dotenv').config()
+ 
+mongoose.connect(process.env.DB_CONN, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
